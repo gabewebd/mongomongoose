0c3c1fc84022fdf7373ea979797c4288d5378d44
gabewebd <gabaivlsqz@gmail.com>
2025-09-27 23:39:50 +0800

    Updated myApp.js

--- myApp.js
+++ myApp.js
@@ -7,6 +7,20 @@ const uri = process.env.MONGO_URI;
 mongoose.connect(uri, { 
   useNewUrlParser: true, 
   useUnifiedTopology: true 
+})
+.then(() => {
+  console.log('MongoDB connected successfully');
+})
+.catch(err => {
+  console.error('MongoDB connection error:', err);
+});
+
+mongoose.connection.on('error', err => {
+  console.error('MongoDB connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB disconnected');
 });
 
 let PersonSchema = new mongoose.Schema({
